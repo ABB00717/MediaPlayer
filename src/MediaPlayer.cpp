73afb1c553e9bf1de2a4d06c7eaf0525d0403ddd
ABB00717 <eric94528@gmail.com>
2024-06-22 11:01:17 +0800

    Update MediaPlayer.cpp

--- src/MediaPlayer.cpp
+++ src/MediaPlayer.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include "GLFW/glfw3.h"
 
-int main() {
-	std::cout << "Hello world" << std::endl;
+int main(int argc, char** argv) {
+	GLFWwindow* window;
+
+    if (!glfwInit()) {
+        std::cout << "ERROR: Couldn't initialize GLFW!" << std::endl;
+        return 1;
+    }
+
+    window = glfwCreateWindow(640, 480, "MediaPlayer", NULL, NULL);
+
+    if (!window) {
+        std::cout << "ERROR: Couldn't create window!" << std::endl;
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+    while (!glfwWindowShouldClose(window)) {
+        std::cout << "Hello" << std::endl;
+        glfwWaitEvents();
+    }
+
 	return 0;
 }
